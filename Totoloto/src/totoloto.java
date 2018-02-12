138bef8b8df6cede44c7c04aca3441fa1e15d137
wortz <jp_pp_hi5@hotmail.com>
2018-02-12 03:39:09 +0000

    finished totoloto

--- Totoloto/src/totoloto.java
+++ Totoloto/src/totoloto.java
@@ -1,41 +1,93 @@
 import java.util.*;
 public class totoloto {
 	
-	
-	
-	public static void getRandoms()
+	public static int getRand()
 	{
-		int numbers[]=new int[5];
 		Random randomo = new Random();
-		int number;
-		int n =0;
-		boolean clone=false;
-		while(n<5)
+		return (randomo.nextInt(49)+1);
+	}
+	
+	public static boolean checkCloned(int[] numbers,int number)
+	{
+		for(int i=0;i<numbers.length;i++)
 		{
-			clone=false;
-			number=(randomo.nextInt(48)+1);
-			for(int i=0;i<numbers.length;i++)
-			{
-				if(numbers[i]==number) {
-					clone=true;
-					break;
-				}
+			if(numbers[i]==number) {
+				return true;
 			}
-			if(clone)
-				continue;
-			numbers[n]=number;
-			n++;
-		
 		}
-		n=0;
+		return false;
+	}
+	
+	public static void printNumbers(int[] numbers)
+	{
+		int n=0;
 		while(n<5) {
 			System.out.println("Number" + (n+1) +" : " + numbers[n]);
 			n++;
 		}
 	}
 	
+	public static int[] getRandoms()
+	{
+		int numbers[]=new int[5];
+		int number;
+		int n =0;
+		boolean clone;
+		while(n<5)
+		{
+			number=getRand();
+			clone=checkCloned(numbers,number); 
+			if(clone)
+				continue;
+			numbers[n]=number;
+			n++;
+		
+		}
+		Arrays.sort(numbers);
+		return numbers;
+	}
+	
+	public static boolean checkBets(int Bets[][],int newBet[])
+	{
+		for(int i=0;i<Bets.length;i++)
+		{
+			if(newBet==Bets[i])
+				return true;
+		}
+		return false;
+	}
+	
+	public static void printBets(int Bets[][])
+	{
+		for(int i=0;i<Bets.length;i++) 
+		{
+			System.out.println("Bet number : " + (i+1) + " ====================================");
+			printNumbers(Bets[i]);
+		}
+	}
+	
+	public static void getBets()
+	{
+		int newBet[];
+		int nOfBets;
+		Scanner input=new Scanner(System.in);
+		System.out.println("How many bets you want to make?");
+		nOfBets=input.nextInt();
+		input.close();
+		int Bets[][]=new int[nOfBets][];
+		for(int i=0;i<nOfBets;) 
+			{
+			newBet=getRandoms();
+			if(checkBets(Bets,newBet))
+				continue;
+			Bets[i]=newBet;
+			i++;
+			}
+		printBets(Bets);
+		}
+	
 	public static void main(String[] args)
 	{
-	getRandoms();
+	getBets();
 	}
 }
